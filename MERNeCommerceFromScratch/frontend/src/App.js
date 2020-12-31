30bb0b67c3da42819a79bef1d2016b2d835dd2d3
Saul Vega <saulv@SaulsMacBook-Pro.local>
2020-12-31 00:23:05 -0700

    finish section 2: 5 react-bootstrap setup, header, and footer components

--- MERNeCommerceFromScratch/frontend/src/App.js
+++ MERNeCommerceFromScratch/frontend/src/App.js
@@ -1,8 +1,20 @@
+//import components
+import Header from './components/Header.js';
+import Footer from './components/Footer.js';
+//styles
+import { Container } from 'react-bootstrap';
+
 const App = () => {
   return (
-    <div className="App">
-      <h1>Welcome To ProShop</h1>
-    </div>
+    <>
+      <Header />
+      <Container>
+        <main className="py-3">
+          <h1>Welcome To ProShop</h1>
+        </main>
+      </Container>
+      <Footer />
+    </>
   );
 };
 
