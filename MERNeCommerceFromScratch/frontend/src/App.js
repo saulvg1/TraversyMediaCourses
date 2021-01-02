42101dc66cc0d8db8daf951b38d0e00a94a148df
Saul Vega <saulv@SaulsMacBook-Pro.local>
2021-01-01 22:50:33 -0700

    section 2: 8. Implementing React Router

--- MERNeCommerceFromScratch/frontend/src/App.js
+++ MERNeCommerceFromScratch/frontend/src/App.js
@@ -1,21 +1,25 @@
+//module imports
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 //import components
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import HomeScreen from './screens/HomeScreen.js';
+import ProductScreen from './screens/ProductScreen.js';
 //styles
 import { Container } from 'react-bootstrap';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
