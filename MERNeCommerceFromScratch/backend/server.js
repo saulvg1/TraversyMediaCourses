04ad254824800d248264c769f886d18f72475098
Saul Vega <saul.vg1@gmail.com>
2021-01-03 22:51:22 -0700

    finish section 3: 14. environment variables

--- MERNeCommerceFromScratch/backend/server.js
+++ MERNeCommerceFromScratch/backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const products = require('./data/products.js');
 
+dotenv.config();
 const app = express();
 
 app.get('/', (req, res) => {
@@ -13,5 +15,9 @@ app.get('/api/products/:id', (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, console.log('server running on port 5000'));
+app.listen(
+  PORT,
+  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
