0ceebf083a1eed4f723803a358945d93a440e45c
Saul Vega <saul.vg1@gmail.com>
2021-01-03 23:31:08 -0700

    finish section 4: 18 adding colors to the console

--- MERNeCommerceFromScratch/backend/config/db.js
+++ MERNeCommerceFromScratch/backend/config/db.js
@@ -6,9 +6,11 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useCreateIndex: true,
     });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
+    );
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`Error: ${error.message}`.red.underline.bold);
     process.exit(1);
   }
 };
