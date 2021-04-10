6eeb957def808f57cc7a1e689f011aadc947bb10
Saul <saul.vg1@gmail.com>
2021-04-10 13:57:35 -0600

    finish section 11 63. next, Prev Song & Progress

---
+++ VanillaJavascriptProjects/musicPlayer/script.js
@@ -0,0 +1,45 @@
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+
+//song titles
+const songs = ['hey', 'summer', 'ukulele'];
+//keep track of song
+let songIndex = 2;
+//initially load song details into Dom
+loadSong(songs[songIndex]);
+//update song details
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+}
+//play song
+function playSong() {
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+  audio.play();
+}
+function pauseSong() {
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+  audio.pause();
+}
+
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play');
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
