7855721ea2f0b11853266c760fe1ab64f035c145
Saul <saul.vg1@gmail.com>
2021-04-15 02:27:48 -0600

    started section 13: 12. typing game | DOM, Intervals, Events

---
+++ VanillaJavascriptProjects/typingGame/script.js
@@ -0,0 +1,102 @@
+const word = document.querySelector('#word');
+const text = document.querySelector('#text');
+const scoreEl = document.querySelector('#score');
+const timeEl = document.querySelector('#time');
+const endgameEl = document.querySelector('#end-game-container');
+const settingsBtn = document.querySelector('#settings-btn');
+const settings = document.querySelector('#settings');
+const settingsForm = document.querySelector('#settings-form');
+const difficultySelect = document.querySelector('#difficulty');
+
+//list for words
+const words = [
+  'shoshone',
+  'exsiccative',
+  ' chapel',
+  'pontificating',
+  'decisively',
+  'congregativeness',
+  ' obeliscal',
+  'nonguidance',
+  'stagecraft',
+  'cave',
+  'nontraction',
+  'transelementated',
+  'inferential',
+  'economically',
+  'unheeled',
+  'disrobed',
+  'noncataclysmal',
+  ' spadelike',
+  'semester',
+  'euhemerus',
+];
+
+let randomWord;
+//init score
+let score = 0;
+
+//init time;
+let time = 10;
+//focus on text on start
+
+text.focus();
+
+//start counting down
+const timeInterval = setInterval(updateTime, 1000);
+
+//update time
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + 's';
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+    gameOver();
+  }
+}
+
+//game over show endscreen
+
+function gameOver() {
+  endgameEl.innerHTML = `
+  
+    <h1>Time Ran Out</h1>
+    <p>Your Final Score is ${score}</p>
+    <button onclick="window.location.reload()">Reload</button>
+  
+  `;
+  endgameEl.style.display = 'flex';
+}
+
+//generate random word from array
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+//add word to dom
+function addWordToDom() {
+  randomWord = getRandomWord();
+  word.innerHTML = randomWord;
+}
+//update score
+function updateScore() {
+  score++;
+  scoreEl.innerHTML = score;
+}
+
+addWordToDom();
+
+//eventlisteners
+
+text.addEventListener('input', (e) => {
+  const insertedText = e.target.value;
+  if (insertedText === randomWord) {
+    addWordToDom();
+    updateScore();
+    //clear
+    e.target.value = '';
+    time += 5;
+    updateTime();
+  }
+});
